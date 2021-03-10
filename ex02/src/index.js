1017847d42daa26fcf0d6a7aa89decdd8616ebcd
Boris Karačić <boris.karacic@outlook.com>
2021-03-10 13:53:46 +0100

    Added index.js file - comments

---
+++ ex02/src/index.js
@@ -0,0 +1,21 @@
+function main() {
+    // Only change code below this line
+
+    // This is my first single-line comment ever!
+
+    /* This
+    is
+    my 
+    first
+    multi-line
+    comment
+    ever!
+    */
+
+    // Only change code above this line
+
+}
+
+main();
+
+module.exports = main;
